0a8c95257be9b2a361555985a08c701f4e039789
07ASN <HASAN.OMAR.20002@GMAIL.COM>
2025-01-05 17:33:05 +0300

    Adding

---
+++ script.js
@@ -0,0 +1,48 @@
+//Q1
+function Car(brand, model, year) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+    this.getDetails = function() {
+        return `Brand: ${this.brand}, Model: ${this.model}, Year: ${this.year}`;
+    };
+}
+const car1 = new Car("BMW", "M3", 2009);
+const car2 = new Car("Hyundai", "Accent", 1994);
+const car3 = new Car("Chevrolet", "Cruze", 2011);
+const cars = [car1, car2, car3];
+
+for (const car of cars) {
+    console.log(car.getDetails());
+}
+
+//Q2
+const persons = {
+    person1: { firstName: "Ahmad", lastName: "Al-Hasan" },
+    person2: { firstName: "Rami", lastName: "Al-Harbi" },
+    person3: { firstName: "Anas", lastName: "Momoni" }
+  };
+  
+  function firstName(personsObj) {
+    const arr = [];
+    let index = 0;
+    for (const key in personsObj) {
+      arr[index] = personsObj[key].firstName;
+      index++;
+    }
+    return arr;
+  }
+console.log(firstName(persons));
+
+
+//Q3
+function objectToArray(obj) {
+    const result = [];
+    for (const key in obj) {
+        result.push(key, obj[key]);
+    }
+    return result;
+}
+
+const input = { firstName: "Hasan", age: 22 };
+console.log(objectToArray(input));
